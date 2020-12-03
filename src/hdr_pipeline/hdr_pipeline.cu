3a2b6a53e71caba913617f435b41850d16957e48
Mehran Jeelani <s8mejeel@stud.uni-saarland.de>
2020-12-03 20:31:36 +0100

    Improved speed

--- src/hdr_pipeline/hdr_pipeline.cu
+++ src/hdr_pipeline/hdr_pipeline.cu
@@ -1,7 +1,7 @@
 #include <cstdint>
 #include "stdio.h"
 #include "math.h"
-
+__device__ float compute(float input_tone, int width, int height);
 __global__ void tonemap_kernel(std::uint32_t* out, const float* in, int width, int height, float exposure)
 {
 	
@@ -12,20 +12,26 @@ __global__ void tonemap_kernel(std::uint32_t* out, const float* in, int width, i
 	if(y< height && x <= width*4-4){
 	float input_tone[3] = {in[y*width*4+x] * exposure,in[y*4*width+x+1]*exposure,in[y*4*width+x+2]*exposure};
 	float output_tone[3];
-	std::uint8_t scaled_output[3];
+	output_tone[0] = compute(in[y*width*4+x]*exposure,width,height);
+	output_tone[1] = compute(in[y*width*4+x+1] * exposure,width,height);
+	output_tone[2] = compute(in[y*width*4+x+2] * exposure,width,height);
+	//std::uint8_t scaled_output[3];
+	/*
 	for(int i=0;i<3;i++){
 		output_tone[i] = input_tone[i]*(0.9036*input_tone[i] + 0.018)/(input_tone[i]*(0.8748*input_tone[i]+0.354)+0.14);
 		if(output_tone[i] <= 0.0031308)
 			output_tone[i] = 12.92 * output_tone[i];
 		else
 			output_tone[i] = 1.055 * powf(output_tone[i],1/2.4) - 0.055;
-		scaled_output[i] = (output_tone[i]>1) ? 255 : rintf(255*output_tone[i]);
+		output_tone[i] = (output_tone[i]>1) ? 255 : rintf(255*output_tone[i]);
 			
 	}
-	out[y*width+x/4] = 0;
-	out[y*width+x/4] |= scaled_output[2] <<16;
-	out[y*width+x/4] |= scaled_output[1]<<8;
-	out[y*width+x/4] |= scaled_output[0];
+	*/
+	out[y*width+x/4] = 0 |(std::uint8_t)(output_tone[2]) <<16 | (std::uint8_t)(output_tone[1]) <<8 | (std::uint8_t)(output_tone[0]);
+	/*
+	out[y*width+x/4] = (std::uint8_t)(output_tone[2]) <<16;
+	out[y*width+x/4] =  (std::uint8_t)(output_tone[1])<<8;
+	out[y*width+x/4] =  (std::uint8_t)(output_tone[0]);
 //	float output_tone = input_tone*(0.9036*input_tone + 0.018)/(input_tone*(0.8748*input_tone+0.354)+0.14);
 //	float output_gamma;
 //	if(output_tone <= 0.0031308)
@@ -33,11 +39,13 @@ __global__ void tonemap_kernel(std::uint32_t* out, const float* in, int width, i
 //	else
 //		output_gamma = 1.055 * pow(output_tone,1/2.4) - 0.055;
 //	std::uint8_t scaled_value = (output_gamma>1) ? 255 : 255*output_gamma;
+	*/
 		
 	}
 	
 	
 }
+
 
 void tonemap(std::uint32_t* out, const float* in, int width, int height, float exposure, float brightpass_threshold)
 {	dim3 blockSize (32,32,1);
@@ -46,4 +54,16 @@ void tonemap(std::uint32_t* out, const float* in, int width, int height, float e
 	tonemap_kernel<<<gridSize, blockSize>>>(out, in, width, height, exposure);
 	cudaDeviceSynchronize();
 //	printf("after tonemapkernel\n");
+}
+
+__device__ float compute(float input_tone, int width, int height){
+	
+	float output_tone =  input_tone*(0.9036*input_tone + 0.018)/(input_tone*(0.8748*input_tone+0.354)+0.14);
+	if(output_tone <= 0.0031308)
+		output_tone = 12.92 * output_tone;
+	else
+		output_tone = 1.055 * powf(output_tone,1/2.4) - 0.055;
+	output_tone = (output_tone>1) ? 255 : rintf(255*output_tone);
+	return output_tone;
+
 }
