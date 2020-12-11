44ab23dbe6668d8853a2c7a0fa43014b699d692c
Mehran Jeelani <s8mejeel@stud.uni-saarland.de>
2020-12-11 10:18:32 +0100

    Assignment 2-2

--- src/hdr_pipeline/hdr_pipeline.cu
+++ src/hdr_pipeline/hdr_pipeline.cu
@@ -1,7 +1,7 @@
 #include <cstdint>
 #include "stdio.h"
 #include "math.h"
-
+__device__ float avgLum;
 __global__ void tonemap_kernel(std::uint32_t* out, const float* in, int width, int height, float exposure)
 {
 	
@@ -29,6 +29,24 @@ __global__ void tonemap_kernel(std::uint32_t* out, const float* in, int width, i
 	
 	
 }
+__global__ void getAvgLum(const float* in, float exposure){
+	__shared__ float ins[16*16];
+	int col = (blockIdx.x*blockDim.x + threadIdx.x)*4;
+	int row = blockIdx.y*blockDim.y + threadIdx.y;
+	if(row< height && col <= width*4-4){
+		ins[threadIdx.y*blockDim.x+threadIdx.x] = logf(0.2126*in[row*4*width+col]+0.7152*in[row*4*width+col+1]+0.0722*in[row*4*width+col+2]);
+		unsigned int t = threadIdx.y*blockDim.x+threadIdx.x;
+		for(unsigned int stride = blockDim.y*blockDim.x>>1;stride>0;stride>>=1){
+			__syncthreads();
+			if(t<stride)
+				ins[t] += ins[t+stride];
+		}
+		if(t==0)
+			avgLum *= expf(ins[t]/(height*width));
+		
+
+	}
+}
 
 
 void tonemap(std::uint32_t* out, const float* in, int width, int height, float exposure, float brightpass_threshold)
@@ -36,7 +54,24 @@ void tonemap(std::uint32_t* out, const float* in, int width, int height, float e
 	dim3 gridSize (width/blockSize.x+1,height/blockSize.y+1,1);
 //	printf("before tonemapkernel\n");	
 	tonemap_kernel<<<gridSize, blockSize>>>(out, in, width, height, exposure);
-	//cudaDeviceSynchronize();
+	cudaDeviceSynchronize();
 //	printf("after tonemapkernel\n");
 }
+void getAvgLum(const float* in,int width,int height,float exposure){
+	float value = 1;
+	throw_error(cudaMemcpyToSymbol(avgLum,&value,sizeof(float)));
+	dim3 blockSize (16,16,1);
+	dim3 gridSize (width/blockSize.x+1,height/blockSize.y+1,1);
+	getAvgLum<<<gridSize, blockSize>>>(d_input_image.get());
+
+	cudaDeviceSynchronize();
+	float result = 0;
+	throw_error(CudaMemcpyFromSymbol(&result,avgLum,sizeof(float)));
+	std::cout<<'result '<<result<<std::endl;
+	float exposure_host =0;
+	throw_error(CudaMemcpyFromSymbol(&exposure_host,exposure,sizeof(float)));
+	exposure_host = exposure_host*0.18/result;
+	throw_error(cudaMemcpyToSymbol(exposure,&exposure_host,sizeof(float)));
+}
+
 
